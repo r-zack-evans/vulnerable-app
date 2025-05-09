36590c45aa7f2e62f4b705b35b727183c0fd926a
r-zack-evans <zack.evans@sourcegraph.com>
2025-05-09 15:54:46 -0500

    removed deprecated js

--- update-routes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Replace EJS rendering with JSON responses
-sed -i.bak 's/res\.json(\([^)]*\));/res.json(\1);/g' src/routes/product.routes.ts
-sed -i.bak 's/res\.json(\'[^'\']*\', { \([^}]*\) })/res.json({ \1 })/g' src/routes/product.routes.ts
-sed -i.bak 's/res\.json(\
